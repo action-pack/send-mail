2a677d4d8737ddeed9c97b0853232918e6646791
Kroese <kroese@users.noreply.github.com>
2024-02-09 13:18:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const nodemailer = require("nodemailer")
-const core = require("@actions/core")
-const glob = require("@actions/glob")
-const fs = require("fs")
-const showdown = require("showdown")
-const path = require("path")
+const nodemailer = require("nodemailer");
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+const fs = require("fs");
+const showdown = require("showdown");
+const path = require("path");
 
 function getText(textOrFile, convertMarkdown) {
-    let text = textOrFile
+    let text = textOrFile;
 
     // Read text from file
     if (textOrFile.startsWith("file://")) {
-        const file = textOrFile.replace("file://", "")
-        text = fs.readFileSync(file, "utf8")
+        const file = textOrFile.replace("file://", "");
+        text = fs.readFileSync(file, "utf8");
     }
 
     // Convert Markdown to HTML
     if (convertMarkdown) {
-        const converter = new showdown.Converter()
-        text = converter.makeHtml(text)
+        const converter = new showdown.Converter();
+        text = converter.makeHtml(text);
     }
 
-    return text
+    return text;
 }
 
 function getFrom(from, username) {
     if (from.match(/.+ <.+@.+>/)) {
-        return from
+        return from;
     }
 
-    return `"${from}" <${username}>`
+    return `"${from}" <${username}>`;
 }
 
 async function getAttachments(attachments) {
-    const globber = await glob.create(attachments.split(',').join('\n'))
-    const files = await globber.glob()
-    return files.map(f => ({ filename: path.basename(f), path: f, cid: f.replace(/^.*[\\\/]/, '')}))
+    const globber = await glob.create(attachments.split(',').join('\n'));
+    const files = await globber.glob();
+    return files.map(f => ({ filename: path.basename(f), path: f, cid: f.replace(/^.*[\\\/]/, '')}));
 }
 
 function sleep(ms) {
@@ -60,7 +60,7 @@ async function main() {
             const url = new URL(connectionUrl)
             switch (url.protocol) {
                 default:
-                    throw new Error(`Unsupported connection protocol '${url.protocol}'`)
+                    throw new Error(`Unsupported connection protocol '${url.protocol}'`);
                 case "smtp:":
                     serverPort = "25"
                     secure = "false"
@@ -84,29 +84,29 @@ async function main() {
             }
         }
 
-        const subject = core.getInput("subject", { required: true })
-        const from = core.getInput("from", { required: true })
-        const to = core.getInput("to", { required: false })
-        const body = core.getInput("body", { required: false })
-        const htmlBody = core.getInput("html_body", { required: false })
-        const cc = core.getInput("cc", { required: false })
-        const bcc = core.getInput("bcc", { required: false })
-        const replyTo = core.getInput("reply_to", { required: false })
-        const inReplyTo = core.getInput("in_reply_to", { required: false })
-        const attachments = core.getInput("attachments", { required: false })
-        const convertMarkdown = core.getInput("convert_markdown", { required: false })
-        const ignoreCert = core.getInput("ignore_cert", { required: false })
-        const priority = core.getInput("priority", { required: false })
-        const nodemailerlog = core.getInput("nodemailerlog", { required: false })
-        const nodemailerdebug = core.getInput("nodemailerdebug", { required: false })
+        const subject = core.getInput("subject", { required: true });
+        const from = core.getInput("from", { required: true });
+        const to = core.getInput("to", { required: false });
+        const body = core.getInput("body", { required: false });
+        const htmlBody = core.getInput("html_body", { required: false });
+        const cc = core.getInput("cc", { required: false });
+        const bcc = core.getInput("bcc", { required: false });
+        const replyTo = core.getInput("reply_to", { required: false });
+        const inReplyTo = core.getInput("in_reply_to", { required: false });
+        const attachments = core.getInput("attachments", { required: false });
+        const convertMarkdown = core.getInput("convert_markdown", { required: false });
+        const ignoreCert = core.getInput("ignore_cert", { required: false });
+        const priority = core.getInput("priority", { required: false });
+        const nodemailerlog = core.getInput("nodemailerlog", { required: false });
+        const nodemailerdebug = core.getInput("nodemailerdebug", { required: false });
 
         // if neither to, cc or bcc is provided, throw error
         if (!to && !cc && !bcc) {
-            throw new Error("At least one of 'to', 'cc' or 'bcc' must be specified")
+            throw new Error("At least one of 'to', 'cc' or 'bcc' must be specified");
         }
-        
+
         if (!serverAddress) {
-            throw new Error("Server address must be specified")
+            throw new Error("Server address must be specified");
         }
 
         const transport = nodemailer.createTransport({
@@ -124,7 +124,7 @@ async function main() {
             debug: nodemailerdebug,
         })
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed(error.message);
     }
 
     var i = 1;
@@ -143,10 +143,10 @@ async function main() {
                 html: htmlBody ? getText(htmlBody, convertMarkdown) : undefined,
                 priority: priority ? priority : undefined,
                 attachments: attachments ? (await getAttachments(attachments)) : undefined,
-            })
+            });
             break;
         } catch (error) {
-            if (!error.message.includes("Try again later,")) { 
+            if (!error.message.includes("Try again later,")) {
                 core.setFailed(error.message)
                 break;
             }
